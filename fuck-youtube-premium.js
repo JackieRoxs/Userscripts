4f0edb555fd2daa445e35167adb446e4bbaaa260
JackieRoxs <mandogy13@gmail.com>
2023-06-28 17:46:05 -0400

    Update fuck-youtube-premium.js

--- fuck-youtube-premium.js
+++ fuck-youtube-premium.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Fuck YouTube Premium
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/JackieRoxs/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       JackieRoxs
 // @match        https://www.youtube.com/watch?v=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
